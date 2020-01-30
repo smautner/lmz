c595af4d64c3eb0e398f7b653dfc31c866debb8d
S Mautner <mautner@cs.uni-freiburg.de>
2020-01-30 17:00:06 +0100

    transpose and grouper added

--- lmz/__init__.py
+++ lmz/__init__.py
@@ -1,8 +1,16 @@
+from itertools import zip_longest
+
 def Map(*x,**y):
     return list(map(*x,**y))
 
 def Zip(*x,**y):
     return list(zip(*x,**y))
 
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
-
+def Transpose(x): 
+    return list(zip(*x))
